a72a7a3bcba68552706441d98977fdc8222c8f5b
mdysput <michal.dysput@gmail.com>
2019-03-02 07:28:38 +0100

    Initial

---
+++ src/MD/BankAccountTestParametrized.java
@@ -0,0 +1,51 @@
+package MD;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class BankAccountTestParametrized {
+
+
+    private BankAccount account;
+    private double amount;
+    private boolean branch;
+    private double expected;
+
+
+    public BankAccountTestParametrized(double amount, boolean branch, double expected) {
+        this.amount = amount;
+        this.branch = branch;
+        this.expected = expected;
+    }
+
+    @org.junit.Before
+    public void setup() {
+        account = new BankAccount("eM", "De", 1000, BankAccount.CHECKING);
+        System.out.println("Running test...");
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> testConditions() {
+        return Arrays.asList(new Object[][]{
+                {100.00, true, 1100},
+                {200.00, true, 1200},
+                {325.14, true, 1325.14},
+                 {1000.00, true, 2000.00}
+        });
+    }
+
+    @org.junit.Test
+    public void deposit() throws Exception {
+        account.deposit(amount, branch);
+        assertEquals(expected, account.getBalance(), .01);
+    }
+}
--- src/MD/Main.java
+++ src/MD/Main.java
@@ -3,6 +3,7 @@ package MD;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("App is running ");
     }
 }
